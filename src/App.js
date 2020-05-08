9a829ff7f5e33435aff9d8e1e15d1511a2043e41
dtofoli1 <daltontofoli1@gmail.com>
2020-05-08 12:00:35 -0300

    Login Screen on going.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LoginScreen from "./pages/login";
+import "./app.css";
 
 const app = () => <LoginScreen></LoginScreen>;
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,20 +1,39 @@
 import React, { Component } from "react";
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Image from "react-bootstrap/Image";
 import logo from "./Logo2x.png";
 
 class loginScreen extends Component {
 	render() {
 		return (
-			<Container fluid>
-				<Row className="justify-content-center">
-					<img src={logo} />
+			<div
+				className="col-4 mx-auto my-auto"
+				styles={{ backgroundColor: "black" }}
+			>
+				<Row className="justify-content-center mt-5">
+					<Image src={logo} alt="" className="align-middle" fluid />
 				</Row>
 				<Row className="justify-content-center">
-					<Button>Botão 1</Button>
+					<Form className="mt-5">
+						<Form.Group controlId="formBasicEmail">
+							<Form.Label>Email address</Form.Label>
+							<Form.Control type="email" placeholder="Enter email" />
+							<Form.Text className="text-muted">
+								We'll never share your email with anyone else.
+							</Form.Text>
+						</Form.Group>
+						<Form.Group controlId="formBasicPassword">
+							<Form.Label>Password</Form.Label>
+							<Form.Control type="password" placeholder="Password" />
+						</Form.Group>
+						<Button variant="warning" type="submit" className="mr-auto">
+							Login
+						</Button>
+					</Form>
 				</Row>
-			</Container>
+			</div>
 		);
 	}
 }
