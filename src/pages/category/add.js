60528d2e93ba7630a19ce446824562fe716836cd
dtofoli1 <daltontofoli1@gmail.com>
2020-06-03 19:05:05 -0300

    add cat and add course

---
+++ src/pages/category/add.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Placeholder from "../../components/img_placeholder.png";
+//import { Link } from "react-router-dom";
+import NavMenu from "../../components/nav";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+//import Row from "react-bootstrap/Row";
+//import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Form from "react-bootstrap/Form";
+
+class addCategory extends Component {
+	render() {
+		return (
+			<div>
+				<div>
+					<NavMenu text={"Nova Categoria"} />
+				</div>
+				<Container fluid>
+					<Image className=" col-sm-7 col-md-6 mt-2" src={Placeholder} fluid />
+					<Form className="mt-5">
+						<Form.Group controldId="newCatName">
+							<Form.Control type="text" placeholder="Nome da categoria" />
+						</Form.Group>
+						<Form.Group controlId="newCatDescription">
+							<Form.Control
+								as="textarea"
+								rows="5"
+								type="text"
+								placeholder="Descrição"
+							/>
+						</Form.Group>
+						<Button variant="success" className="mr-auto">
+							SALVAR
+						</Button>
+					</Form>
+				</Container>
+			</div>
+		);
+	}
+}
